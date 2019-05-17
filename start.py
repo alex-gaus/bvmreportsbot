891bf319bc5d0930a7e462b2484c0d3a818215fd
gobitodic <alexander@gaus-fellbach.de>
2019-05-17 17:18:39 +0200

    .

--- start.py
+++ start.py
@@ -1,10 +1,4 @@
 from bvmreportbot import main
 import os
 print(os.getcwd())
-os.chdir("home")
-print(os.getcwd())
-os.chdir("gobitodic")
-print(os.getcwd())
-os.chdir("bvmreportsbot")
-print(os.getcwd())
 main()
