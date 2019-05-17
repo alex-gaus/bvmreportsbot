5d57d826fdf68e77e7533ce07462509992b91f62
gobitodic <alexander@gaus-fellbach.de>
2019-05-17 16:18:48 +0200

    Change in merge_pdf

--- merge_pdf.py
+++ merge_pdf.py
@@ -4,7 +4,7 @@ logging.basicConfig(level=logging.INFO)
 def merge_pdf(mergeList,userpdflocation,userfilename):
         logging.info("merge_pdf started")
         #Sets the scripts working directory to the location of the PDFs
-        os.chdir(userpdflocation)
+        # os.chdir(userpdflocation)
         # pdf2merge = []
         # filelist_unsorted = os.listdir(".") 
         # filelist = sorted(filelist_unsorted)
@@ -15,13 +15,13 @@ def merge_pdf(mergeList,userpdflocation,userfilename):
         pdfWriter = PyPDF2.PdfFileWriter()
 
         for filename in mergeList:
-                pdfFileObj = open(filename,"rb")
+                pdfFileObj = open("pdfs/%s"%(filename),"rb")
                 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
         #Opening each page of the PDF
                 for pageNum in range(pdfReader.numPages):
                         pageObj = pdfReader.getPage(pageNum)
                         pdfWriter.addPage(pageObj)
-        os.chdir("..")
+        # os.chdir("..")
         #save PDF to file, wb for write binary
         pdfOutput = open("pdfs/"+userfilename+".pdf", "wb")
         #Outputting the PDF
